0dbba288f99fd948ea56f3a216f2efc80cf2a2df
Daniil Samoylov <daniil.samoylov@auror.co>
2018-10-05 14:41:32 +1300

    Setup percy

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,14 @@
-import { configure } from '@storybook/react';
+import { configure, getStorybook, setAddon } from '@storybook/react';
 import 'todomvc-app-css/index.css'
 import 'storybook-chromatic';
+import createPercyAddon from '@percy-io/percy-storybook';
+const { percyAddon, serializeStories } = createPercyAddon();
+
+setAddon(percyAddon);
 
 function loadStories() {
   require('../components/stories/');
 }
 
 configure(loadStories, module);
+serializeStories(getStorybook);
